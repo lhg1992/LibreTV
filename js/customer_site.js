dfc8fb0cb5b0786ef81e434d19048612ad463521
lhg1992 <383724525@qq.com>
2025-08-25 23:30:39 +0400

    20250825
    增加主页资源站

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,8 +1,20 @@
 const CUSTOMER_SITES = {
+    baofeng1:{
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风资源'
+    },
+    baofeng2:{
+        api: 'http://by.bfzyapi.com/api.php/provide/vod',
+        name: '暴风备用'
+    },
+    maotai:{
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台资源'
+    },
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }
+    },
 };
 
 // 调用全局方法合并
