fdaedcb0d8a1cd9dcf0918bec37599b751684084
lhg1992 <383724525@qq.com>
2025-08-26 00:06:42 +0400

    20250826
    1. 新增多个资源
    2. 更改默认资源

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,20 +1,48 @@
 const CUSTOMER_SITES = {
-    baofeng1:{
+    tyyszy:{
+        api:'https://tyyszyapi.com/api.php/provide/vod',
+        name: '天涯影视'
+    },
+    bfzy:{
         api: 'https://bfzyapi.com/api.php/provide/vod',
         name: '暴风资源'
     },
-    baofeng2:{
+    dyttzy:{
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂'
+    },
+    bfzy2:{
         api: 'http://by.bfzyapi.com/api.php/provide/vod',
         name: '暴风备用'
     },
-    maotai:{
+    maotaizy:{
         api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
         name: '茅台资源'
     },
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    dbzy:{
+        api: 'https: //caiji.dbzy.tv/api.php/provide/vod',
+        name: '豆瓣资源'
     },
+    iqyzy:{
+        api: 'https://iqiyizyapi.com/api.php/provide/vod',
+        name: '爱奇艺资源'
+    },
+    bdzy:{
+        api: 'https://api.apibdzy.com/api.php/provide/vod',
+        name: '百度资源'
+    },
+    qvod:{
+        api: 'https://gayapi.com/api.php/provide/vod',
+        name: '快播资源'
+    },
+    ytzy:{
+        api: 'https://apiyutu.com/api.php/provide/vod',
+        name: '玉兔资源'
+    },
+    hsckzy:{
+        api: 'https://hsckzy.vip/api.php/provide/vod',
+        name: '红手指'
+    }
 };
 
 // 调用全局方法合并
